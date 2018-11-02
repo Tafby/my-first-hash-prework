d44ed68146b54a963dde8c44a360586280fb506e
Tiffany Carter <tafby88@gmail.com>
2018-11-02 03:17:31 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,12 +38,4 @@ def adding
 shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] = 4
 shipping_manifest
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
 end
